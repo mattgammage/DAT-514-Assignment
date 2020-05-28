1e48c722fdcac08cf437a1107a9582cc02361493
mattgammage <44403657+mattgammage@users.noreply.github.com>
2020-05-28 14:18:30 +0100

    Add files via upload

---
+++ DAT 514/Server.js
@@ -0,0 +1,12 @@
+var express = require(express);
+var app = express ()
+
+var
+
+const port = process.env.port || 
+const mongoURL = process.env.mongoURL ||
+
+
+
+app.listen(port)
+console.log ('Server listening on port)
